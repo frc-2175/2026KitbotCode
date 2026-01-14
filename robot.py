5830b5e3162b4c105659d045043c2aa08844961c
2175 L12 <info@fightingcalculators.org>
2026-01-13 20:33:21 -0600

    Added Intake and Shooter Code

--- robot.py
+++ robot.py
@@ -1,17 +1,28 @@
 import wpilib
 from subsytems.drivetrain import Drivetrain
+from subsytems.intakeandshooter import IntakeAndShooter
 
 class myRobot(wpilib.TimedRobot):
     def robotInit(self):
         self.drivetrain = Drivetrain()
+        self.intakeandshooter = IntakeAndShooter()
         self.leftJoystick = wpilib.Joystick(0)
         self.rightJoystick = wpilib.Joystick(1)
         self.gamepad = wpilib.Joystick(2)
 
     def teleopPeriodic(self):
-        driveSpeed = -self.gamepad.getRawAxis(1)
-        steerSpeed = -self.gamepad.getRawAxis(4)
+        #Drive
+        driveSpeed = -self.leftJoystick.getRawAxis(1)
+        steerSpeed = -self.rightJoystick.getRawAxis(4)
         self.drivetrain.drive(driveSpeed, steerSpeed)
+
+        #Intake
+        intakeSpeed = 0.5 if self.gamepad.getRawButton(1) else 0
+        self.intakeandshooter.setIntakeSpeed(intakeSpeed)
+
+        #Shooter
+        shooterSpeed = 0.5 if self.gamepad.getRawButton(2) else 0
+        self.intakeandshooter.setShooterSpeed(shooterSpeed)
 
     def disabledInit(self):
         self.drivetrain.drive(0, 0)
---
+++ subsytems/intakeandshooter.py
@@ -0,0 +1,12 @@
+import rev
+
+class IntakeAndShooter:
+    def __init__(self):
+        self.intakeMotor = rev.SparkMax(20, rev.SparkLowLevel.MotorType.kBrushless)
+        self.shooterMotor = rev.SparkMax(21, rev.SparkLowLevel.MotorType.kBrushless)
+
+    def setIntakeSpeed(self, intakeSpeed: float):
+        self.intakeMotor.set(intakeSpeed)
+
+    def setShooterSpeed(self, shooterSpeed: float):
+        self.shooterMotor.set(shooterSpeed)
